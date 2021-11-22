886eaf5ebdc225e47f2df23abaf3552c8d4ceefa
koebe1 <koe.be.pay@web.de>
2021-11-22 15:05:06 +0100

    blank again -> new tutorial for router v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "./App.css";
-import { Header } from "./components/Header";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-        <Header />
+    
       </div>
-      {/* routes syntax necessary for react router v6 */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
   );
 }
 
