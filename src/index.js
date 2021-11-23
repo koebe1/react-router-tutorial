82f3f92e58bbb5c8f54332a3cc936d8eab7dcaa7
koebe1 <koe.be.pay@web.de>
2021-11-23 11:08:08 +0100

    useParams

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  Outlet
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import "./Home.css";
 import { Home } from "./components/Home";
 import { Learn } from "./components/Learn";
+import CourseId from "./components/CourseId";
 import reportWebVitals from "./reportWebVitals";
 
 import Courses from "./components/Courses";
@@ -26,7 +21,9 @@ ReactDOM.render(
         {/* <Navigate to="/learn" redirects */}
         <Route path="/myapps" element={<Navigate replace to="/learn" />} />
         <Route path="/learn" element={<Learn />}>
-          <Route path="courses" element={<Courses />} />
+          <Route path="courses" element={<Courses />}>
+            <Route path=":courseId" element={<CourseId />} />
+          </Route>
           <Route path="bundles" element={<Boundles />} />
         </Route>
       </Routes>
